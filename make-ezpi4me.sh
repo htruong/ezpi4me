b4f717273e6339d87f3a073db2bc7406e4bc7ec3
Huan <jimmies@anna.lan>
2018-02-14 05:52:14 -0500

    Initial commit

---
+++ make-ezpi4me.sh
@@ -0,0 +1,176 @@
+#!/bin/bash
+
+# ezpi4me - Raspberry Pi ME Cleaner image creation script
+# Written by Huan Truong <htruong@tnhh.net>, 2018
+# This script is licensed under GNU Public License v3
+
+IMAGE_FILE=2017-11-29-raspbian-stretch-lite.zip
+IMAGE_FILE_UNZIPPED=2017-11-29-raspbian-stretch-lite.img
+IMAGE_FILE_CUSTOMIZED=ezpi4me.img
+IMAGE_URL=http://downloads.raspberrypi.org/raspbian_lite/images/raspbian_lite-2017-12-01/2017-11-29-raspbian-stretch-lite.zip
+IMAGE_SIZE_RAW=1858076672
+TEMP_CHROOT_DIR=/mnt/raspbian-temp
+
+
+#########################################################
+# Support functions
+#########################################################
+
+bail_and_cleanup() {
+    kpartx -d $1 
+    rm $2
+}
+
+check_command_ok() {
+    if ! [ -x "$(command -v $1)" ]; then
+        echo 'Error: $1 is not installed. Please install it.' >&2
+        exit 1
+    fi
+}
+
+check_root() {
+    # make sure we're root
+    if [ "$EUID" -ne 0 ]; then 
+        echo "Please run this script as using sudo/as root, otherwise it can't continue."
+        exit
+    fi
+}
+
+check_dependencies() {
+    check_command_ok kpartx
+    check_command_ok parted
+    check_command_ok qemu-arm-static
+    check_command_ok chroot
+}
+
+get_unzip_image() {
+    #get raspberry image
+    if [ -f ${IMAGE_FILE} ]; then
+        echo "Image file ${IMAGE_FILE} is already here, skip download. To re-download, please remove it."
+    else
+        wget -O${IMAGE_FILE} ${IMAGE_URL}
+    fi
+    if ! [ -f ${IMAGE_FILE_UNZIPPED} ]; then
+        unzip ${IMAGE_FILE}
+    fi
+    cp ${IMAGE_FILE_UNZIPPED} ${IMAGE_FILE_CUSTOMIZED}
+}
+
+resize_raw_image() {
+    IMAGE_SIZE_ACTUAL=$(wc -c < "${IMAGE_FILE_CUSTOMIZED}")
+    if [ ${IMAGE_SIZE_ACTUAL} -gt ${IMAGE_SIZE_RAW} ]; then
+        echo "Image seems already resized, or something is wrong."
+        echo "If the image doesn't work, try removing the .img and try again."
+        return
+    fi
+    echo "Resizing image"
+    
+    #resize image
+    dd if=/dev/zero bs=1M count=128 >> ${IMAGE_FILE_CUSTOMIZED}
+
+    parted ${IMAGE_FILE_CUSTOMIZED} <<'END'
+    resizepart 2 -1s
+    quit
+END
+
+}
+
+set_up_loopdevs() {
+    # mount the resized partition
+    LOOPPARTSRET=$(kpartx -v -a ${IMAGE_FILE_CUSTOMIZED})
+    LOOPPARTSID=`echo ${LOOPPARTSRET} | sed 's/add map //' | cut -f1 -d' ' | sed 's/p1$//'`
+
+    #echo "-- LoopFS setup --\n${LOOPPARTSRET}"
+    echo "The loop device is ${LOOPPARTSID}"
+
+    # it should have two partitions at /dev/mapper
+    if ! [ -L /dev/mapper/${LOOPPARTSID}p1 ]; then
+        echo "Couldn't find the loopdev partitions at /dev/mapper/${LOOPPARTSID}!"
+        bail_and_cleanup /dev/${LOOPPARTSID} ${IMAGE_FILE_CUSTOMIZED}
+        exit 1
+    fi
+
+    echo "Found the loopdev partitions at /dev/mapper/${LOOPPARTSID}!"
+    LOOPDEVPARTS=/dev/mapper/${LOOPPARTSID}
+
+    e2fsck -f ${LOOPDEVPARTS}p2
+    e2fsck ${LOOPDEVPARTS}p2
+
+    mount_chroot_dirs ${LOOPDEVPARTS} ${LOOPPARTSID}
+    
+    # now we should have a 
+    
+    # ld.so.preload fix
+    sed -i 's/^/#CHROOT /g' ${TEMP_CHROOT_DIR}/etc/ld.so.preload
+
+    # copy qemu binary
+    cp `which qemu-arm-static` ${TEMP_CHROOT_DIR}/usr/bin/
+    
+    cp support-scripts/ezpi4me-check-chip ${TEMP_CHROOT_DIR}/usr/local/sbin
+    cp support-scripts/ezpi4me-rom-backup ${TEMP_CHROOT_DIR}/usr/local/sbin
+    cp support-scripts/ezpi4me-rom-clean ${TEMP_CHROOT_DIR}/usr/local/sbin
+    cp support-scripts/ezpi4me-rom-reflash ${TEMP_CHROOT_DIR}/usr/local/sbin
+    
+    cp coreboot/coreboot.bin ${TEMP_CHROOT_DIR}/home/pi/coreboot.bin
+    
+    cp support-scripts/customize-image-pi.sh ${TEMP_CHROOT_DIR}/root/
+    
+    
+    # phew, customize it
+    chroot ${TEMP_CHROOT_DIR} /bin/bash /root/customize-image-pi.sh
+    #chroot ${TEMP_CHROOT_DIR} /bin/bash
+    
+    # undo ld.so.preload fix
+    sed -i 's/^#CHROOT //g' ${TEMP_CHROOT_DIR}/etc/ld.so.preload
+    
+    umount_chroot_dirs
+    umount_loop_dev /dev/${LOOPPARTSID}
+
+    echo "If you reach here, it means the image is ready. :)"
+
+}
+
+
+mount_chroot_dirs() {
+    echo "Mounting CHROOT directories"
+    mkdir -p ${TEMP_CHROOT_DIR}
+    
+    mount -o rw ${1}p2 ${TEMP_CHROOT_DIR}
+    mount -o rw ${1}p1 ${TEMP_CHROOT_DIR}/boot
+
+    # mount binds
+    mount --bind /dev ${TEMP_CHROOT_DIR}/dev/
+    mount --bind /sys ${TEMP_CHROOT_DIR}/sys/
+    mount --bind /proc ${TEMP_CHROOT_DIR}/proc/
+    mount --bind /dev/pts ${TEMP_CHROOT_DIR}/dev/pts
+
+    if  ! [ -f ${TEMP_CHROOT_DIR}/etc/ld.so.preload ]; then
+        echo "I didn't see ${TEMP_CHROOT_DIR}/etc/ folder. Bailing!"
+        umount_chroot_dirs
+        umount_loop_dev /dev/$2
+        exit 1
+    fi
+    
+}
+
+umount_chroot_dirs() {
+    umount ${TEMP_CHROOT_DIR}/{dev/pts,dev,sys,proc,boot,}
+    sync
+}
+
+umount_loop_dev() {
+    kpartx -d $1
+}
+
+#########################################################
+
+
+check_dependencies
+check_root
+get_unzip_image
+resize_raw_image
+set_up_loopdevs
+
+
+
+
---
+++ support-scripts/customize-image-pi.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# ezpi4me
+
+# Raspberry Pi ME Cleaner image customization script
+# Written by Huan Truong <htruong@tnhh.net>, 2018
+# This script is licensed under GNU Public License v3
+
+###############################################################################
+
+print_banner() {
+    echo "---- WELCOME TO THE RASPBERRY PI IMAGE CUSTOMIZER --------------"
+    sleep 1
+    echo " Congratulations, we have gone a long way."
+    sleep 1
+    echo " I will prepare some software for you, sit tight."
+    sleep 1
+    echo ""
+    echo ""
+    echo ""
+}
+
+change_bootconfig() {
+    sed -i 's/ quiet init\=.*$/ modules-load=dwc2,g_serial/' /boot/cmdline.txt
+    echo "dtoverlay=dwc2" >> /boot/config.txt
+    echo "dtparam=spi=on" >> /boot/config.txt
+}
+
+get_deps() {
+    apt update
+    #apt upgrade
+    apt install git flashrom
+}
+
+mark_script_run() {
+    touch /etc/customizer_done
+}
+
+get_mecleaner() {
+    git clone https://github.com/corna/me_cleaner
+    cp me_cleaner/me_cleaner.py /usr/local/bin
+}
+
+
+###############################################################################
+
+if [ -f /etc/customizer_done ]; then
+    echo "This script has been run before. Nothing to do."
+    exit 0
+fi
+
+cd 
+
+print_banner
+
+change_bootconfig
+
+get_deps
+
+get_mecleaner
+
+mark_script_run
